47a770100d5824bd7a32fdff0859d0127edc04fe
Toufik Fettous <fettous.toufik@outlook.com>
2021-06-30 17:09:56 +0200

    second commit

---
+++ phase1.sh
@@ -0,0 +1,32 @@
+
+#!/bin/sh
+
+# Je récupere le hostname du serveur
+HOSTNAME="$(hostname)"
+
+# Si la machine s'appelle control
+if [ "$HOSTNAME" = "control" ]; then
+	# J'installe puppet dessus
+	apt-get install -y \
+		puppet-master
+else
+	# J'installe puppet dessus
+	apt-get install -y \
+		puppet
+
+	systemctl restart puppet
+
+	cat > /etc/puppet/puppet.conf <<-MARK
+    [main]
+    ssldir = /var/lib/puppet/ssl
+    certname = $HOSTNAME
+    server = control
+    environment = production
+    [master]
+    vardir = /var/lib/puppet
+    cadir = /var/lib/puppet/ssl/ca
+    dns_alt_names = puppet
+	MARK
+
+	puppet agent --test
+fi
